b3cf448ae3ea51a584423893b9141cbedb140f40
ulzana <almazova_u@iuca.kg>
2026-09-21 21:52:40 +0600

    Initial commit.

---
+++ app/src/main/java/com/example/laboratory_work_2_ulzana/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.laboratory_work_2_ulzana
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.laboratory_work_2_ulzana.ui.theme.Laboratory_work_2_ulzanaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Laboratory_work_2_ulzanaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laboratory_work_2_ulzanaTheme {
+        Greeting("Android")
+    }
+}
